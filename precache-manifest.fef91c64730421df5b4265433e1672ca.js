d5f2f3fa6b2e8a9d6f14b6b9299f728ea78af7f1
Sanchit Manoj Wakekar <sanchit.wakekar@synapse.com>
2019-12-16 12:57:14 +0530

    Updates

---
+++ precache-manifest.fef91c64730421df5b4265433e1672ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c00cc677c56ce8c97998145b152a47",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "771562753a4bf44b8025",
+    "url": "/newsapp/static/css/main.b6895c52.chunk.css"
+  },
+  {
+    "revision": "a0c9963a7d2d1ed1d5e7",
+    "url": "/newsapp/static/js/2.726ba625.chunk.js"
+  },
+  {
+    "revision": "771562753a4bf44b8025",
+    "url": "/newsapp/static/js/main.460a590c.chunk.js"
+  },
+  {
+    "revision": "a951f16037118e59aca7",
+    "url": "/newsapp/static/js/runtime-main.b16c04eb.js"
+  }
+]);
